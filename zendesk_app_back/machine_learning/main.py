29ccdf6d68d1c40bd5149cd6b13cde46f6053f83
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2016-07-05 17:06:00 +0200

    First python script

---
+++ zendesk_app_back/machine_learning/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+# parse arguments and create an array of comments
+ticket = []
+for arg in sys.argv[1:]:
+    ticket.append(arg)
+
+# do some stuff
+
+# return result
+print "Bonjour ! Je suis là pour vous aider."
+print "Salut ! Je suis là pour t'aider."
+print "Tu pues... Au revoir !"
