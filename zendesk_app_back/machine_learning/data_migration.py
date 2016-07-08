616abb7d8a8533a470256b835a07128d77cad920
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2016-07-08 12:52:22 +0200

    Adding pre-processing template for machine learning

---
+++ zendesk_app_back/machine_learning/data_migration.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+""" This file create a question.txt and answer.txt from a raw ticket """
+
+
+def chkdir():
+    """ Create folder data/raw if it doesn't exists """
+    pass
+
+def create_arrays():
+    """ Create questions = [] ( array of array ) and answers = [] ( array ) """
+    pass
+
+def save_arrays():
+    """ Save arrays questions and answer in data/arrays """
+    pass
+
+def load_arrays():
+    pass
+
+
+if __name__ == '__main__':
+    pass
