15ca2e0e9181a3fa6edf3814593907a59067c2d8
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2016-07-20 14:06:38 +0200

    Adding processing scripts

---
+++ zendesk_app_back/machine_learning/sendit/doc2vec.py
@@ -0,0 +1,63 @@
+#coding: utf8
+from __future__ import print_function
+from gensim.models.doc2vec import Doc2Vec, TaggedLineDocument
+from gensim.models import doc2vec
+import logging
+import numpy as np
+from sklearn.cross_validation import train_test_split, StratifiedKFold
+import pickle
+from gensim import corpora, models
+from collections import defaultdict
+import operator
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+def training_doc2vec(path_sentences, size=10000):
+    documents = TaggedLineDocument(path_sentences)
+    model = Doc2Vec(documents, size, window=15, min_count=10)
+    model.save('./doc2vec/dataset')
+    
+def get_doc2vec_vectors(path_sentences):
+    model = Doc2Vec.load('./ml/doc2vec')
+    emb_dim = model.vector_size
+    with open(path_sentences, 'r+') as f:
+        lines = f.readlines()
+        sentences = np.zeros((len(lines), emb_dim))
+        i = 0
+        for line in lines:
+            sentences[i] = model.docvecs[i]
+            i += 1
+    return sentences
+
+def formatting_ml_input(path_sentences, path_labels):
+    sentences = get_doc2vec_vectors(path_sentences)
+    with open(path_labels, 'r+') as f:
+        lines = f.readlines()
+        labels = np.zeros((len(lines), 1))
+        i = 0
+        for line in lines:
+            labels[i] = line
+            i += 1
+    print("saving formated input")
+    with open('./ml/input/formated/doc2vec/sentences.npy', 'wb') as f:
+        np.save(f, sentences)
+    with open('./ml/input/formated/doc2vec/labels.npy', 'wb') as f:
+        np.save(f, labels)
+    print('shape of sentences', sentences.shape)
+    print('shape of labels', labels.shape)
+    return sentences, labels
+
+if __name__ == '__main__':
+    path_sentences = './ressources/dataset_AA.txt'
+#    path_labels = './ml/input/labels.txt'
+    training_doc2vec(path_sentences)
+    
+#    model = Doc2Vec.load('./ml/doc2vec')
+#    print(model.similar_by_word('purple', topn=3, restrict_vocab=None))
+#    print(model.similar_by_word('blue', topn=3, restrict_vocab=None))
+#    print(model.similar_by_vector([1,2]))
+#    print(model.most_similar(positive=['blue', 'purple'], negative=[], topn=3, restrict_vocab=None))
+#    print(model.doesnt_match("cereal breakfast dinner lunch".split()))
+#    sentences, labels = formatting_ml_input(path_sentences, path_labels)
+#    print(TaggedLineDocument("./ressources/HarryPotter"))
+
---
+++ zendesk_app_back/machine_learning/sendit/pre-processing.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import os, urllib, json, shutil, sys, time, csv, re, codecs, unicodedata, glob
+from nltk.corpus import stopwords
+
+
+def split_sentences(txt):
+    """
+    Split paragraph into array of sentences
+    :param txt: one or more paragraph
+    :return: array of sentences
+    """
+    pass
+
+def parse_txt(txt):
+    """
+    Splits a string into an lean string without punctiation, carriage returns & stopwords.
+    """
+    punctuation = ['(', ')', ':', ';', '«', '»', ',', '-', '!', '.', '?', '/', '[', ']', '{', '}',
+                   '#', '"', '*', '-', "`", '"', '>>', '|', '/', '*', '•', ' ', "d'", "j'",
+                   "t'", "l'", "s'", "n'", "qu'", "c'"]
+    carriage_returns = ['\n', '\r\n']
+    word_regex = "^[a-zàâçéèêëîïôûùüÿñæœ/+ .-]+$"
+    stop_words_set = set()
+    stopwordsfile = stopwords.words('french')
+    for word in stopwordsfile:  # a stop word in each line
+        word = word.replace("\n", '')
+        word = word.replace("\r\n", '')
+        stop_words_set.add(word)
+    clean_txt = ''
+    words = txt.split()
+    for word in words:
+        # lower case
+        word = word.lower()
+        # remove punctuation & carriage returns
+        for punc in punctuation + carriage_returns:
+            word = word.replace(punc, ' ').strip(' ')
+        # check if it is normal letters
+        if not re.match(word_regex, word):
+            word = None
+        # remove stopwords
+        if word and (word not in stop_words_set) and (len(word) > 1):
+            try:
+                words = unicodedata.normalize('NFKD', unicode(word, 'utf-8')).encode('ASCII', 'ignore').split()
+                for word in words:
+                    if word and (word not in stop_words_set) and (len(word) > 1):
+                        clean_txt = clean_txt + word + ' '
+            except:
+                pass
+    return clean_txt
+
+if __name__ == '__main__':
+    ### for Deep learning ####
+    # datadir = './data/Stanford - IMDB review sentiment analysis dataset/ang/test'
+    # os.mkdir('./dl')
+    # os.mkdir('./dl/tmp')
+    # os.mkdir('./dl/tmp/models_saved')
+    # os.mkdir('./dl/input')
+    # os.mkdir('./dl/input/test')
+    # os.mkdir('./dl/input/formated')
+    # os.mkdir('./dl/input/formated/test')
+    # path_sentences = './dl/input/test/sentences.txt'
+    # path_labels = './dl/input/test/labels.txt'
+    # #### for Machine learning ####
+    # datadir = './data/kaggle - Bag of Words Meets Bags of Popcorn/train.csv'
+    #
+    # os.mkdir('./ml')
+    # os.mkdir('./ml/tmp')
+    # os.mkdir('./ml/tmp/models_saved')
+    # os.mkdir('./ml/input')
+    # os.mkdir('./ml/input/formated')
+    #
+    # path_sentences = './ml/input/sentences.txt'
+    # path_labels = './ml/input/labels.txt'
+    ################################
+
+    print(parse_txt("Coucou, ceci est un message de test pour savoir si j'suis pas un toto\nsCeci est la deuxième phrase !"))
